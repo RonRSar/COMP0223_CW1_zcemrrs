012ca45014147bcddb9b560dd63fca393165195e
RonitS <zcemrrs@ucl.ac.uk>
2023-11-12 02:11:32 +0000

    read_rail_network implemented and tested

--- utilities.py
+++ utilities.py
@@ -1,9 +1,12 @@
 import csv
+from pathlib import Path
 
 def read_rail_network(filepath):
     """
     A function to read CSV files and import them for use. 
     """
+    assert isinstance(filepath, Path) , 'data type incorrect for filepath'
+
     reader = open(filepath, newline='')
     rail_network = csv.reader(reader, delimiter=';')
     return rail_network
