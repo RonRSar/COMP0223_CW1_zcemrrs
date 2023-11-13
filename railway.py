05f83a0105c001506414f159a2baad2a6ca8850a
RonitS <zcemrrs@ucl.ac.uk>
2023-11-13 22:36:54 +0000

    fixed plot_fares_to bug,  journey was wrong way

--- railway.py
+++ railway.py
@@ -3,6 +3,9 @@ import math
 
 
 def fare_price(distance, different_regions, hubs_in_dest_region):
+    '''
+    Function to calculate the fare_price based on the direct distance between stations, region and number of hubs in region
+    '''
     fare_price = 1 + distance*math.exp(-distance/100)*(1+(different_regions*hubs_in_dest_region)/10)
     return fare_price
 
@@ -27,7 +30,7 @@ class Station:
         assert len(self.crs) == 3, 'CRS is incorrect length, expected 3 letters'
         assert -90 <= self.lat <= 90 , 'Latitude is not in -90 to 90 range'
         assert -180 <= self.lon <= 180, 'Longitude is not in -180 to 180 range'
-        assert int(hub) == 0 or int(hub) == 1, 'input incorrect for hub, expected 0 or 1' 
+        assert int(hub) == 0 or int(hub) == 1, 'input incorrect for hub, expected bool or 0/1' 
  
 
     def __str__(self):
@@ -47,19 +50,21 @@ class Station:
         #splitting equation for easier readability and error checking
         lat_dif = (other_station.lat - self.lat)*0.5
         lon_dif = (other_station.lon - self.lon)*0.5
+
         #multiplying by pi/180 to convert from radian to degrees
         k_1 = math.sqrt((math.sin(lat_dif*math.pi/180))**2 + 
                         ((math.cos(self.lat*math.pi/180)*math.cos(other_station.lat*math.pi/180))*(math.sin(lon_dif*math.pi/180))**2))
-        R = 6371
+        
+        R = 6371 #declaring R as constant in case earth explodes and loses width
         distance = abs(2*R*math.asin(k_1)) #abs to prevent negative distances
         return distance
 
 
 class RailNetwork:
     def __init__(self, stations):
-        self.stations = stations
+        self.stations = stations #station input is list
 
-        keys = []
+        keys = [] #CRS are unique identifiers
         for row in range(int(len(self.stations))):
             keys.append(self.stations[row].crs)
 
@@ -70,13 +75,16 @@ class RailNetwork:
         count = 0
         for station in self.stations:
             stations_dict[keys[count]] = station
-            count += 1
+            count += 1 #manually counting to prevent use of another loop structure
 
         #convert stations to dictionary
         self.stations = stations_dict
 
 
     def regions(self):
+        '''
+        Function finds the unique regions in a rail network
+        '''
         region_list = []
         for key in self.stations.keys():
             region_list.append(str(self.stations[key].region))
@@ -85,10 +93,17 @@ class RailNetwork:
         return unique_regions
 
     def n_stations(self):
+        '''
+        Function counts number of stations in rail network
+        '''
         n_stations = int(len(self.stations))
         return n_stations
 
     def hub_stations(self, region='all'):
+        '''
+        Function produces list of all hubmstations in rail network. 
+        region can be set to specify an area, or left blank to return all hubs
+        '''
 
         #verify region is string
         assert isinstance(region,str), 'data type incorrect for region, expected str'
@@ -101,18 +116,24 @@ class RailNetwork:
 
         if region == 'all': #default value
             return hub_stations
+        elif [region in self.regions()]:
+            return hub_stations[region]
         else:
-            return hub_stations[region] #to do: add case where region implemented isn't in list
+            raise KeyError('Region not in network') #case where region is inputted wrong or does not exist
+        
 
     def closest_hub(self, s):
+        '''
+        Function takes a station and finds the closest hub to it in the same region.
+        '''
         hubs_in_region = self.hub_stations(s.region)
         min = 1e7
         for station in hubs_in_region:
             dist = station.distance_to(s)
-            if dist <= min: #if equidistant then alphabetical
+            if dist <= min: #if equidistant then goes alphabetical
                 min = dist
-                #if min == 0: #todo: floating point error
-                #   continue: #station is already hub
+                # if min == 0:
+                #   continue #station is already hub, no need to codeify or flag since closest_hub is therefore station
                 closest_hub = station
             elif min == 1e7:
                 closest_hub = []
@@ -123,29 +144,36 @@ class RailNetwork:
     
     def journey_planner(self, start, dest):
         '''
-        start, dest = crs inputs
-        returns list of stations in journey'''
+        Function takes the start and destination stations as crs inputs and returns a list of stations in the journey from start to end
+        '''
+        # find closest hub
         start_closest_hub = self.closest_hub(self.stations[start])
         end_closest_hub = self.closest_hub(self.stations[dest]) 
 
         journey = []
-        journey.append(self.stations[start])
+        journey.append(self.stations[start]) #start is always included
 
+        #append if journey is inbetween regions
         if start_closest_hub != self.stations[start] and start_closest_hub.region != end_closest_hub.region:
             journey.append(start_closest_hub)
    
         if end_closest_hub != self.stations[dest] and end_closest_hub.region != start_closest_hub.region:
             journey.append(end_closest_hub)
 
-        journey.append(self.stations[dest]) 
+        journey.append(self.stations[dest]) #end is always included
 
         return journey
         
 
     def journey_fare(self, start, dest, summary=False):
+        '''
+        Function takes start and destination as crs values and returns the cost of the journey. 
+        Summary is true to output more information and show the journey passage. 
+        '''
 
         journey = self.journey_planner(start, dest)
         journey_fare = 0
+
         if len(journey) == 2: # for simple 2 leg case
             distance = self.stations[start].distance_to(self.stations[dest])
             if self.stations[start].region == self.stations[dest].region:
@@ -198,21 +226,21 @@ class RailNetwork:
 
     def plot_fares_to(self, crs_code, save, **args):
         """
-        A function for plot_fares_to
-        takes crs_code as input and produces histogram of fare prices from all other stations
+        Function to plot all fares to one specific station. 
+        Takes crs_code of station as input and produces histogram of fare prices from all other stations
         save is true for image to be saved locally to computer
         **args are used for the hist plot
         """
         fares = []
         for station in self.stations.values(): 
             if crs_code != station.crs: #ensure not checking self
-                if [station.region in self.hub_stations()]: #ensure station can be travelled to
-                    fares.append(self.journey_fare(crs_code, station.crs))
-                continue
+                # if [station.region in self.hub_stations()]: #ensure station can be travelled to
+                fares.append(self.journey_fare(station.crs, crs_code))
+                # continue
             continue
         
         station_name = self.stations[crs_code].name.replace(" ", "_")
-        plt.title(f"Fare price to {station_name}")
+        plt.title(f"Fare prices to {station_name}")
         plt.xlabel("Fare price (Pound)")
 
         if args != None: 
@@ -221,9 +249,10 @@ class RailNetwork:
             plt.hist(fares)
 
         if save:
-            plt.savefig(f"./Fare_price_to_{station_name}.png")
+            plt.savefig(f"./Fare_prices_to_{station_name}.png")
         
-        plt.show()
+        plt.waitforbuttonpress(0)
+
         
         return
 
